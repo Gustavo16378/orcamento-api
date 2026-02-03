85d0573b7b83509bd0bc12aa90407c7e0f80f146
Gustavo16378 <gustavo16378@gmail.com>
2026-02-03 19:38:56 -0300

    refactor: DTOs, controllers, downgrade spring version e integração Swagger

--- src/main/java/com/orcamento/api/repository/QuoteRequestRepository.java
+++ src/main/java/com/orcamento/api/repository/QuoteRequestRepository.java
@@ -3,7 +3,11 @@ package com.orcamento.api.repository;
 import com.orcamento.api.entity.QuoteRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
+import java.util.List;
 
 public interface QuoteRequestRepository extends JpaRepository<QuoteRequest, UUID> {
-    
+
+    List<QuoteRequest> findAllByDeletedAtIsNotNull();
+
+    List<QuoteRequest> findAllByDeletedAtIsNull();
 }
